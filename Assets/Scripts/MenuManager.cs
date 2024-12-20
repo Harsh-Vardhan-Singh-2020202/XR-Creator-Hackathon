9dcd12b9710e33e01d08cf5fcc4d2b0976ae2c2f
Harsh-Vardhan-Singh-2020202 <88623300+Harsh-Vardhan-Singh-2020202@users.noreply.github.com>
2024-12-21 03:48:10 +0530

    App updated

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -2,9 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
 
 public class MenuManager : MonoBehaviour
 {
+    [SerializeField] private XRReferenceImageLibrary newReferenceLibrary;
+
+    void Start()
+    {
+        ResetARTracker();
+    }
+
     public void ChangeScene(int sceneIndex)
     {
         SceneManager.LoadScene(sceneIndex);
@@ -13,5 +22,26 @@ public class MenuManager : MonoBehaviour
     public void Quit()
     {
         Application.Quit();
+    }
+
+    public void ResetARTracker()
+    {
+        ARTrackedImageManager trackedImageManager = FindObjectOfType<ARTrackedImageManager>();
+
+        if (trackedImageManager == null)
+        {
+            Debug.Log("ARTrackedImageManager not found in the scene.");
+            return;
+        }
+
+        // Set the appropriate reference library
+        if (newReferenceLibrary != null)
+        {
+            trackedImageManager.referenceLibrary = newReferenceLibrary;
+        }
+        else
+        {
+            Debug.LogWarning("New Reference Library is not assigned.");
+        }
     }
 }
